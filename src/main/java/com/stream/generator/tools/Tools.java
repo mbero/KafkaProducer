5e511312708f2ea17b3ff5196a139e314a9a0115
mbero <mberendt546@gmail.com>
2017-06-24 15:53:14 +0200

    Added POJO to JSON String parser

---
+++ src/main/java/com/stream/generator/tools/Tools.java
@@ -0,0 +1,18 @@
+package com.stream.generator.tools;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Tools {
+
+	/**
+	 * Transforms given simple object (POJO) for JSON String
+	 * @param object
+	 * @return
+	 * @throws JsonProcessingException
+	 */
+	public String getJSONStringFromGivenObject(Object object) throws JsonProcessingException{
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonInString = mapper.writeValueAsString(object);
+		return jsonInString;
+	}
+}
---
+++ src/test/java/com/stream/generator/tools/ToolsTest.java
@@ -0,0 +1,33 @@
+package com.stream.generator.tools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.stream.generator.bms.SingleBMSReadRecord;
+
+public class ToolsTest {
+
+	private Tools tools;
+
+	@Before
+	public void setUp() throws Exception {
+		tools = new Tools();
+	}
+
+	@Test
+	public void testGetJsonFromGivenObject() {
+		SingleBMSReadRecord singleBMSRecord = new SingleBMSReadRecord("2018294", "some value", "some id", "readTag_id");
+		String jsonString = "";
+		try {
+			jsonString = tools.getJSONStringFromGivenObject(singleBMSRecord);
+		} catch (JsonProcessingException e) {
+			System.out.println("Error during processing Object into JSON string");
+			e.printStackTrace();
+		}
+		assertNotEquals("", jsonString);
+	}
+
+}
