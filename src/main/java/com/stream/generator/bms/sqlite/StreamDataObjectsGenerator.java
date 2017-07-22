25cef864f244159a8fb059d0ff6391ffff1d9dcb
mbero <mberendt546@gmail.com>
2017-07-22 16:14:55 +0200

    Updated list of parameters

--- src/main/java/com/stream/generator/bms/sqlite/StreamDataObjectsGenerator.java
+++ src/main/java/com/stream/generator/bms/sqlite/StreamDataObjectsGenerator.java
@@ -95,6 +95,7 @@ public class StreamDataObjectsGenerator {
 	public ArrayList<SingleBMSReadRecord> getWholeListOfBMSReadRecords() {
 		return listOfReadRecord;
 	}
+	
 
 	public List<SingleBMSReadRecord> getListOfSingleBMSRecordsFromProperJSONFile(String fullPathToJSONFile)
 			throws JsonParseException, JsonMappingException, IOException, ParseException, ClassNotFoundException {
--- src/main/java/com/stream/generator/main/Main.java
+++ src/main/java/com/stream/generator/main/Main.java
@@ -2,6 +2,7 @@ package com.stream.generator.main;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.json.simple.parser.ParseException;
 
@@ -12,26 +13,41 @@ import com.stream.generator.kafka.KafkaProducer;
 import com.stream.generator.tools.Tools;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		if (args.length < 4) {
-			System.err.println(
-					"Parameters should consist of : <kafka_bootstrap_server_adress> <zookeper_adress> <topic_name> <direct_path_to_sqlite_db_file>");
+		if (args.length < 6) {
+			System.err.println("Parameters should consist of : " + "<kafka_bootstrap_server_adress> "
+					+ "<zookeper_adress> (NONE if dont wanna use Zookeper parameter)" + "<topic_name>"
+					+ "<type_of_data_source> - JSON or SQLITE"
+					+ "<path_to_data_source> (full path to proper JSON file or SQLITE.DB file"
+					+ "<time_between_meessages> - in seconds, for example : 1,2 etc");
 			System.exit(1);
 		}
+
 		String kafkaBootstrapServerAdress = args[0]; // f.e "localhost:9092"
 		String zookeperAdress = args[1]; // f.e //"localhost:2181"
 		String topicName = args[2]; // f.e 'tweets-text'
-		String directPathToSQLiteDBFile = args[3]; // "E:/PCSS/bms_analytics_workspace/StreamGenerator/BMS_DB.db"
+		String typeOfDataSource = args[3];
+		String pathToDataSource = args[4];
+		String timeBetweenMessages = args[5];
+		Integer timeBetweenMessagesInt = Integer.valueOf(timeBetweenMessages);
+
 		KafkaProducer kafkaProducer = new KafkaProducer();
 		kafkaProducer.initializeProducer(kafkaBootstrapServerAdress, zookeperAdress);
 		StreamDataObjectsGenerator bmsDataStreamGenerator = new StreamDataObjectsGenerator();
 		List<SingleBMSReadRecord> bmsDataRecords = null;
 		try {
-			bmsDataRecords = bmsDataStreamGenerator.getListOfSingleBMSRecordsFromProperJSONFile(
-					"E:\\PCSS\\bms_analytics_workspace\\StreamGenerator\\tests\\resultsJSONFile.json");
+			if (typeOfDataSource.equals("JSON")) {
+				bmsDataRecords = bmsDataStreamGenerator.getListOfSingleBMSRecordsFromProperJSONFile(pathToDataSource);
+			} else if (typeOfDataSource.equals("SQLITE")) {
+				bmsDataStreamGenerator.putAllDataFromTrendTableIntoMemory(pathToDataSource);
+				bmsDataRecords = bmsDataStreamGenerator.getWholeListOfBMSReadRecords();
+			}
+
 		} catch (ClassNotFoundException | IOException | ParseException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Overall type exception during ");
+			e.printStackTrace();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		List<SingleBMSReadRecord> randomlySelectedBMSReadRecords = bmsDataStreamGenerator
@@ -39,17 +55,19 @@ public class Main {
 		int i = 0;
 		for (SingleBMSReadRecord singleBMSRead : randomlySelectedBMSReadRecords) {
 			i += 1;
-			// SingleBMSReadRecord singleBMSReadRecord =
-			// bmsDataStreamGenerator.getRandomRecordFromWholeListOfBMSReadRecords();
 			String singleBMSRecordString = null;
 			try {
 				singleBMSRecordString = Tools.getJSONStringFromGivenObject(singleBMSRead);
+				kafkaProducer.produceMessage(topicName, String.valueOf(i), singleBMSRecordString);
+				System.out.println(singleBMSRecordString);
+				TimeUnit.SECONDS.sleep(timeBetweenMessagesInt);
 			} catch (JsonProcessingException e) {
-				// TODO Auto-generated catch block
+				System.out.println("JsonProcessingException - error during getting JSON from single BMS read object");
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				System.out.println("JsonProcessingException - error during getting JSON from single BMS read object");
 				e.printStackTrace();
 			}
-			kafkaProducer.produceMessage(topicName, String.valueOf(i), singleBMSRecordString);
-			System.out.println(singleBMSRecordString);
 		}
 		kafkaProducer.closeProducer();
 	}
